13047941997f0f77332e85ab621cf6b4a502fa67
Paul <baconpaul@users.noreply.github.com>
2025-09-25 17:39:58 -0400

    Explicitly pass CMAKE_BINARY_DIR to subsidary inno cmake (#10)
    What a pain. but i think this does it

--- innosetup/install_innosetup.cmake
+++ innosetup/install_innosetup.cmake
@@ -1,13 +1,16 @@
+if (NOT TARGET_DIR)
+    message(FATAL_ERROR "Target Dir not set in cmake step")
+endif()
 file(
     DOWNLOAD
         "https://files.jrsoftware.org/is/6/innosetup-6.5.4.exe"
-        "${CMAKE_BINARY_DIR}/innosetup-6.5.4.exe"
+        "${TARGET_DIR}/innosetup-6.5.4.exe"
     EXPECTED_HASH
         SHA256=FA73BF47A4DA250D185D07561C2BFDA387E5E20DB77E4570004CF6A133CC10B1
 )
 
 execute_process(
     COMMAND
-        "${CMAKE_BINARY_DIR}/innosetup-6.5.4.exe" /VERYSILENT /CURRENTUSER
-        /DIR=${CMAKE_BINARY_DIR}/innosetup-6.5.4
+        "${TARGET_DIR}/innosetup-6.5.4.exe" /VERYSILENT /CURRENTUSER
+        /DIR=${TARGET_DIR}/innosetup-6.5.4
 )
